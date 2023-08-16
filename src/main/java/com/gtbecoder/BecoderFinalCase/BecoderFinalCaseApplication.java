51ccfd8d1e6d3834e5864576e688fec4a6227fc7
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2023-08-16 12:55:42 +0300

    Initial commit

---
+++ src/main/java/com/gtbecoder/BecoderFinalCase/BecoderFinalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.gtbecoder.BecoderFinalCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecoderFinalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecoderFinalCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtbecoder/BecoderFinalCase/BecoderFinalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtbecoder.BecoderFinalCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecoderFinalCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
